7264a6bbdc8042cee206a0f17b21af28b1cb7382
marionino.obog <marioo@magenic.com>
2022-05-20 02:29:09 +0800

    [mobog] major changes

--- src/main/java/com/magenic/mobog/exercise2app/exceptions/EntityNotFoundException.java
+++
@@ -1,4 +0,0 @@
-package com.magenic.mobog.exercise2app.exceptions;
-
-public class EntityNotFoundException extends RuntimeException{
-}
--- src/main/java/com/magenic/mobog/exercise2app/exceptions/InvalidRequestException.java
+++
@@ -1,5 +0,0 @@
-package com.magenic.mobog.exercise2app.exceptions;
-
-public class InvalidRequestException extends RuntimeException {
-
-}
